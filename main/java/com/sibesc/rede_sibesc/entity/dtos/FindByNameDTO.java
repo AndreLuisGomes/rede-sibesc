d9ac326e1ba4d60e05f244a0ad4c3c5651089ab4
AndreLuisGomes <andreluisgomes09@gmail.com>
2025-07-16 20:58:42 -0300

    reset: Zerando repositório

--- main/java/com/sibesc/rede_sibesc/entity/dtos/FindByNameDTO.java
+++
@@ -1,6 +0,0 @@
-package com.sibesc.rede_sibesc.entity.dtos;
-
-import org.springframework.boot.context.properties.bind.DefaultValue;
-
-public record FindByNameDTO(String name, @DefaultValue("0") Integer page, @DefaultValue("5") Integer itensPerPage) {
-}
